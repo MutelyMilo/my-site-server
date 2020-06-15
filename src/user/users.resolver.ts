3d1450f348014a87816ef1970966b52c77fdbfbf
Phoenix <mutelymilo@gmail.com>
2020-06-15 23:40:10 +0800

    Install class-validator

---
+++ src/user/users.resolver.ts
@@ -0,0 +1,30 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { RegisterInput } from './dto/register.input';
+import { LoginInput } from './dto/login.input';
+
+@Resolver()
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => String)
+  hello() {
+    return 'hello world';
+  }
+  
+  @Query(() => [User], { nullable: true })
+  async getUsers() {
+    return await this.usersService.all();
+  }
+
+  @Mutation(() => User)
+  register(@Args('data') registerData: RegisterInput): Promise<User> {
+    return this.usersService.register(registerData);
+  }
+
+  @Mutation(() => User)
+  login(@Args('data') loginData: LoginInput): Promise<User> {
+    return this.usersService.login(loginData);
+  }
+}
