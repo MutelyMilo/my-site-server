9f4fd2fa3dcccb36dd2abcd78c5f76d35a405129
Phoenix <mutelymilo@Gmail.com>
2020-07-14 16:25:45 +0800

    Update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import {typeOrmConfig} from 'src/config/typeORM'
+import {typeOrmConfig} from './config/TypeORM'
 // 渲染前端静态代码需要的
 // import { ServeStaticModule } from '@nestjs/serve-static';
 // import { join } from 'path';
